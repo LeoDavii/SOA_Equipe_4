5985099b5d9465b79044b3aefaa9fff5b53bacec
Laryssa Carvalho <laryssa.carvalho@ambevtech.com.br>
2022-03-25 15:31:41 -0300

    Singleton pattern implementation on database class

--- SOA_Escola_Alunos/SOA_Escola_Alunos/Services/StudentService.cs
+++ SOA_Escola_Alunos/SOA_Escola_Alunos/Services/StudentService.cs
@@ -1,7 +1,6 @@
 ﻿using SOA_Escola_Alunos.Database.Repositories;
 using SOA_Escola_Alunos.Models;
 using System;
-using System.Collections.Generic;
 using System.IO;
 
 namespace SOA_Escola_Alunos.Services
@@ -12,52 +11,51 @@ namespace SOA_Escola_Alunos.Services
 
         public StudentService()
         {
-            var filePath = Directory.GetCurrentDirectory() + @"..\..\..\..\Database\Students.json";
-            _studentRepository = new BaseRepository<Student>(filePath);
+            _studentRepository = new BaseRepository<Student>();
         }
         public void Create(string name)
-        {            
-            if(name is not null)            
-                _studentRepository.Add(new Student(name));            
+        {
+            if (name is not null)
+                _studentRepository.Add(new Student(name));
         }
 
         public void Update(int id)
         {
             var student = _studentRepository.GetById(id);
 
-            if(student == null)
+            if (student == null)
             {
                 Console.WriteLine($"Não existe um aluno com esse Id.");
                 return;
-            }            
-            
+            }
+
             Console.WriteLine("Insira o nome do aluno: ");
 
             student.Name = Console.ReadLine();
 
             _studentRepository.Update(student);
-            Console.WriteLine($"Aluno atualizado com sucesso!");            
+            Console.WriteLine($"Aluno atualizado com sucesso!");
         }
 
-        public void GetAll() 
+        public void GetAll()
         {
             var students = _studentRepository.GetAll();
 
-            foreach(var student in students)            
-                Console.WriteLine($"Id: {student.Id} Nome: {student.Name}");            
+            foreach (var student in students)
+                Console.WriteLine($"Id: {student.Id} Nome: {student.Name}");
         }
-        public void Delete(int id) 
+        public void Delete(int id)
         {
             var student = _studentRepository.GetById(id);
-            
-            if(student == null)
+
+            if (student == null)
             {
                 Console.WriteLine($"Não existe um aluno com esse Id.");
                 return;
             }
 
             _studentRepository.Delete(student);
-            Console.WriteLine($"Aluno deletado com sucesso!");            
+            Console.WriteLine($"Aluno deletado com sucesso!");
         }
     }
 }
